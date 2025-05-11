2e9bce3c5a40dd141d7f158cf2a9a20a0ae24ee7
faieqah <faieqah.siddiqui@live.com>
2025-05-12 04:07:40 +0500

    Added User Context and dashboard layout

---
+++ app/(dashboard)/dashboard/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className="pt-40 pl-40 text-red-500">
+      Loading Dashboard... {/* You can add a spinner, skeleton, or text */}
+    </div>
+  )
+}
+
+export default loading
