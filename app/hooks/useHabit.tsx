2010676437988fb6a2d5062151300d4e917768b9
faieqah <faieqah.siddiqui@live.com>
2025-09-16 02:19:14 +0500

    Added daily and weekly streaks

--- app/hooks/useHabit.tsx
+++ app/hooks/useHabit.tsx
@@ -1,69 +1,70 @@
-'use client'
+"use client";
 import { useEffect, useState } from "react";
 //import { createClient } from "@/supabase/clients"; // This should return the browser-side supabase client
-import { Habit,HabitWithLogs } from "../constants/types";
+import { Habit, HabitWithLogs } from "../constants/types";
 import { supabase } from "@/app/utils/supabase/createClient";
 import { calculateStreak } from "../utils/calculateStreak";
 
-
 export const useHabits = (
-        sortBy: keyof Habit = 'name', // default sort by name not`created_at`
-        sortOrder: 'asc' | 'desc' = 'asc',   // default ascending order
-        limit:number = 5,
-        pageNumber:number = 1,
-        searchTerm: string="",
-        category: string ="",
-        frequency: string="",
-        selectedDate: Date | null,
-        completed: boolean | null = null   
+  sortBy: keyof Habit = "name", // default sort by name not`created_at`
+  sortOrder: "asc" | "desc" = "asc", // default ascending order
+  limit: number = 5,
+  pageNumber: number = 1,
+  searchTerm: string = "",
+  category: string = "",
+  frequency: string = "",
+  selectedDate: Date | null,
+  completed: boolean | null = null
+) => {
+  //const [habits, setHabits] = useState<Habit[]>([]);
+  const [habits, setHabits] = useState<HabitWithLogs[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [totalHabits, setTotalHabits] = useState(0);
 
+  console.log("useHabits hook called");
+  console.log("Selected DDATEE Normal:", selectedDate);
 
-)=>{
-    //const [habits, setHabits] = useState<Habit[]>([]);
-    const [habits, setHabits] = useState<HabitWithLogs[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError]= useState<string | null>(null);
-    const [totalHabits, setTotalHabits]= useState(0);
+  //console.log("Selected DDATEE hook Formatted:", selectedDate?.toISOString().split("T")[0])
+  console.log(
+    "Selected DDATEE hook Formatted:",
+    selectedDate?.toLocaleDateString("en-CA")
+  );
 
-    console.log("useHabits hook called");
-    console.log("Selected DDATEE Normal:", selectedDate)
+  const fetchHabits = async () => {
+    setLoading(true);
+    setError(null);
 
-    //console.log("Selected DDATEE hook Formatted:", selectedDate?.toISOString().split("T")[0])
-    console.log("Selected DDATEE hook Formatted:", selectedDate?.toLocaleDateString("en-CA"))
+    //const supabase = createClient();
 
+    //console.log("Supabase client:", supabase);
+    //console.log("Habits per page in the hook: ",limit);
 
-    const fetchHabits = async () =>{
+    const from = (pageNumber - 1) * limit;
+    const to = from + (limit - 1);
 
-      setLoading(true);
-      setError(null);
+    //***********MAIN QUERY BEFORE HABIT LOG JOIN************
+    //let query = supabase.from('Habit').select('*', {count: 'exact'}).order(sortBy, {ascending:sortOrder === 'asc'}).range(from,to);
 
-        //const supabase = createClient();
-
-        //console.log("Supabase client:", supabase);
-        //console.log("Habits per page in the hook: ",limit);
-
-        const from = (pageNumber-1) * limit;
-        const to = from + (limit-1)
-
-
-        //***********MAIN QUERY BEFORE HABIT LOG JOIN************
-        //let query = supabase.from('Habit').select('*', {count: 'exact'}).order(sortBy, {ascending:sortOrder === 'asc'}).range(from,to);
-
-
-        let query = supabase.from('Habit').select(
-          `*,
-          HabitLog${completed ==true ? "!inner": ""}(
+    let query = supabase
+      .from("Habit")
+      .select(
+        `*,
+          HabitLog${completed == true ? "!inner" : ""}(
             id,
             date,
             completed
-          )`,{count:'exact'}).order(sortBy,{ascending:sortOrder==='asc'}).range(from,to);
+          )`,
+        { count: "exact" }
+      )
+      .order(sortBy, { ascending: sortOrder === "asc" })
+      .range(from, to);
 
+    // if (completed !== null) {
+    //       query = query.eq("HabitLog.completed", completed);
+    //     }
 
-// if (completed !== null) {
-//       query = query.eq("HabitLog.completed", completed);
-//     }
-
-// ✅ Completed = true → only habits with at least one completed log
+    // ✅ Completed = true → only habits with at least one completed log
     if (completed === true) {
       query = query.eq("HabitLog.completed", true);
     }
@@ -73,79 +74,79 @@ export const useHabits = (
       query = query.or("HabitLog.completed.eq.false,HabitLog.id.is.null");
     }
 
-          // if(selectedDate){
-          //   //const formattedDate = selectedDate.toISOString().split("T")[0]; // YYYY-MM-DD
-          //   const formattedDate = selectedDate.toLocaleDateString("en-CA"); 
-          //   query = query.eq('HabitLog.date',formattedDate);
-          // }
-        // Case-insensitive partial match for category
-        if (category.trim() !== "" &&  category!=="All") {
-          query = query.ilike('category', `%${category}%`);
-        }
-
-        // Case-insensitive partial match for category
-        if (frequency.trim() !== "" &&  frequency!=="All") {
-          query = query.ilike('frequency', `%${frequency}%`);
-        }
-
-        if(searchTerm.trim() !==""){
-          query=query.ilike('name',`%${searchTerm}%`)
-        }
-
-        const {data,count,error}= await query;
-
-       
-
-        //console.log("Supabase returned:", { data, error });
-
-        if(error){
-            //console.error(error);
-            setError(error.message?? "Failed to fetch habits");
-        }
-        else{
-
-          //habit streaks
-
-          const enrichedHabits = (data as HabitWithLogs[]).map((habit)=> {
-            const streak = calculateStreak(habit.HabitLog || []);
-            return {...habit, streak};
-          });
-
-          setHabits(enrichedHabits);
-
-
-            //setHabits(data);
-            setTotalHabits(count||0);
-            //console.log("Hook Habits: ",data);
-             console.log("Hook Habits: ",enrichedHabits);
-        }
-        setLoading(false);
+    // if(selectedDate){
+    //   //const formattedDate = selectedDate.toISOString().split("T")[0]; // YYYY-MM-DD
+    //   const formattedDate = selectedDate.toLocaleDateString("en-CA");
+    //   query = query.eq('HabitLog.date',formattedDate);
+    // }
+    // Case-insensitive partial match for category
+    if (category.trim() !== "" && category !== "All") {
+      query = query.ilike("category", `%${category}%`);
     }
 
-    useEffect(() => {
-      fetchHabits(); // example: sort by name ascending
+    // Case-insensitive partial match for category
+    if (frequency.trim() !== "" && frequency !== "All") {
+      query = query.ilike("frequency", `%${frequency}%`);
+    }
 
-      const handleHabitsUpdated = () => {
-        //console.log("Habit created event detected");
-        fetchHabits();
-      };
-      const handleHabitLogged = () => {
-        //console.log("Habit created event detected");
-        fetchHabits();
-      };
+    if (searchTerm.trim() !== "") {
+      query = query.ilike("name", `%${searchTerm}%`);
+    }
 
-      window.addEventListener("habits-updated", handleHabitsUpdated);
-      window.addEventListener("habit-logged", handleHabitLogged);
+    const { data, count, error } = await query;
 
+    //console.log("Supabase returned:", { data, error });
 
+    if (error) {
+      //console.error(error);
+      setError(error.message ?? "Failed to fetch habits");
+    } else {
+      //habit streaks
 
-      return () => {
-        window.removeEventListener("habits-updated", handleHabitsUpdated);
-        window.removeEventListener("habit-logged", handleHabitLogged);
+      const enrichedHabits = (data as HabitWithLogs[]).map((habit) => {
+        const streak = calculateStreak(habit,habit.HabitLog || []);
+        return { ...habit, streak };
+      });
 
-      };
-    }, [sortOrder, limit, pageNumber, searchTerm, category, frequency, selectedDate, completed]);
+      setHabits(enrichedHabits);
 
-    return {habits, loading, error, totalHabits, fetchHabits};
+      //setHabits(data);
+      setTotalHabits(count || 0);
+      //console.log("Hook Habits: ",data);
+      console.log("Hook Habits: ", enrichedHabits);
+    }
+    setLoading(false);
+  };
 
-}
+  useEffect(() => {
+    fetchHabits(); // example: sort by name ascending
+
+    const handleHabitsUpdated = () => {
+      //console.log("Habit created event detected");
+      fetchHabits();
+    };
+    const handleHabitLogged = () => {
+      //console.log("Habit created event detected");
+      fetchHabits();
+    };
+
+    window.addEventListener("habits-updated", handleHabitsUpdated);
+    window.addEventListener("habit-logged", handleHabitLogged);
+
+    return () => {
+      window.removeEventListener("habits-updated", handleHabitsUpdated);
+      window.removeEventListener("habit-logged", handleHabitLogged);
+    };
+  }, [
+    sortOrder,
+    limit,
+    pageNumber,
+    searchTerm,
+    category,
+    frequency,
+    selectedDate,
+    completed,
+  ]);
+
+  return { habits, loading, error, totalHabits, fetchHabits };
+};
--- app/utils/calculateStreak.tsx
+++ app/utils/calculateStreak.tsx
@@ -1,49 +1,98 @@
+import { Habit, HabitLog } from "../constants/types";
+
+export function calculateStreak(
+  habit: Habit,
+  habitLogs: HabitLog[],
+): number {
+  if (!habitLogs || habitLogs.length === 0) return 0;
+
+  //Filter completed logs and map to JS Date object
+
+  const completedDates = habitLogs
+    .filter((log) => log.completed)
+    .map((log) => new Date(log.date).toLocaleDateString("en-CA"));
+
+  //console.log("CompletedDates from streak function:", completedDates);
+
+  if (completedDates.length === 0) return 0;
+
+  const sortedDates = completedDates
+      .map((d) => new Date(d))
+      .sort((a, b) => b.getTime() - a.getTime());
+    console.log("CompletedDates from streak function sorted:", sortedDates);
+
+  const today = new Date().toLocaleDateString("en-CA");
+  //console.log("Today:",today);
+
+  let streak = 0;
 
 
-import { HabitLog } from "../constants/types";
-
-export function calculateStreak(habitLogs: HabitLog[], frequency: "Daily"|"Weekly"|"Monthly"="Daily"):number{
-    if(!habitLogs || habitLogs.length===0) return 0;
-
-
-    //Filter completed logs and map to JS Date object
-    // const completedDates = habitLogs.filter((log)=>log.completed).map((log)=> new Date(log.date).toDateString());
-
-
-     const completedDates = habitLogs.filter((log)=>log.completed).map((log)=> new Date(log.date). toLocaleDateString("en-CA")
-);
-
-     console.log("CompletedDates from streak function:",completedDates);
-
-    if(completedDates.length===0) return 0;
-    
-    const today= new Date().toLocaleDateString("en-CA");
-    //console.log("Today:",today);
-
-    let streak = 0;
-
-    let current= new Date();
-
-    if(!completedDates.includes(today)){
-        current.setDate(current.getDate()-1);
-    }
+// 🔽 ADD HELPER FUNCTION HERE
+function hasLogOnOrBetween(start: Date, end: Date): boolean {
+  return completedDates.some(d => {
+    const logDate = new Date(d);
+    return logDate <= start && logDate >= end;
+  });
+}
 
 
 
-    console.log("Current:",current);
+  let current = new Date();
 
+  if (!completedDates.includes(today)) {
+    current.setDate(current.getDate() - 1);
+  }
 
-    while(true){
-        const currentStr = current.toLocaleDateString("en-CA");
-        if(completedDates.includes(currentStr)){
-            streak++;
+  console.log("Current:", current);
 
-            current.setDate(current.getDate()-1);
-        }
-        else{
-            break;
-        }
+  if (habit.frequency === "Daily") {
+    //console.log("CompletedDates from streak function:", completedDates);
+
+    while (true) {
+      const currentStr = current.toLocaleDateString("en-CA");
+      if (completedDates.includes(currentStr)) {
+        streak++;
+
+        current.setDate(current.getDate() - 1);
+      } else {
+        break;
+      }
     }
     return streak;
+  }
+  else if (habit.frequency === "Weekly") {
+    console.log(
+      "CompletedDates from streak function unsorted:",
+      completedDates
+    );
 
+    
+
+    console.log("Habit: ", habit.name);
+
+    //const lastLog = sortedDates[0];
+
+    let windowStart = new Date(); // today
+    let windowEnd = new Date();
+    windowEnd.setDate(windowStart.getDate() - 6); // 7-day window
+
+    while (true) {
+      if(!hasLogOnOrBetween(windowStart,windowEnd)) break;
+
+      streak++;
+
+      //Move window back by 7 days
+
+      windowStart.setDate(windowStart.getDate()-7);
+      windowEnd.setDate(windowEnd.getDate()-7);
+
+    }
+
+    return streak;
+  }
+  else{
+
+  }
+
+  return 0;
 }
